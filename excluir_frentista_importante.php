c7a43ef45bc05f3a22dfce093cac6e4feb45eeb7
Eduardo Rodrigues <136652355+Duardo-Dev@users.noreply.github.com>
2023-09-21 15:56:17 -0300

    Update excluir_frentista_importante.php

--- excluir_frentista_importante.php
+++ excluir_frentista_importante.php
@@ -9,10 +9,11 @@
     <?php
         session_start();
         include_once 'connect.php';
-
+        //pega o id do usuário, apartir da session.
         $id = $_SESSION['id_frentista'];
-
+        //delete primeiramente o usuário das notas fiscais que ele esta incluida.
         $sql_dados = "DELETE from nota_fiscal where id_frentista = '$id'";
+        //Depois ele deleta o usuário.
         $sql = "DELETE from frentista where id_frentista = '$id'";
 
         
