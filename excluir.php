780443ff033da17073b70e3571f1a7c58a243ad3
Eduardo Rodrigues <136652355+Duardo-Dev@users.noreply.github.com>
2023-09-21 15:02:12 -0300

    Update excluir.php

--- excluir.php
+++ excluir.php
@@ -16,16 +16,16 @@
     if(!empty($_GET['id_frentista'])){
         $id = $_GET['id_frentista'];
 
-        $_SESSION['id_frentista'] = $id;
+        $_SESSION['id_frentista'] = $id;//pegando das sessions do id do usuário.
 
         
         $sql = "SELECT * FROM frentista WHERE id_frentista = $id";
        
         $result  = $con->query($sql);
 
-        $sql_nota = "SELECT * from nota_fiscal where id_frentista = '$id'";
+        $sql_nota = "SELECT * from nota_fiscal where id_frentista = '$id'";//interligando as notas fiscais com o id do frentista
         $result_nota = $con -> query($sql_nota);
-
+        // se existir alguma nota fiscal existente na table,recebera uma mensagem para confirmar.
         if($result_nota -> num_rows > 0 ){
 
             echo "
@@ -33,16 +33,16 @@
                 <span> se sim, <a href='excluir_frentista_importante.php'> clique aqui </a></span>
             ";
         } else {
-            
+            //verificar se na table de usuário existe esse usuário.
             if($result-> num_rows > 0){
 
-                
+                //deletar da table do frentista.
                 $sqldelete = "DELETE FROM frentista  WHERE id_frentista = $id";
 
                 
                 $resultdelete  = $con->query($sqldelete);
 
-                
+                //redirecionar o administrador para a tela de mudar as informações.
                 header('location:mudar_info.php');
             } 
         }
