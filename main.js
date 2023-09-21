582e051e4be7cd80c61300501d0d12632e77fd83
Eduardo Rodrigues <136652355+Duardo-Dev@users.noreply.github.com>
2023-09-21 13:07:53 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 
 function validarSenhaForca(){
 
-	
+	//pegar o id do post da senha criado na tela de cadastro.
 	var senha = document.getElementById('password_user').value;
 
-
+        //criando a variável de força.
 	var forca = 0;
 
 	
 	document.getElementById("impSenha").innerHTML = "Senha " + senha;
 
 
-	
+	//se for += de 4 letras, ou -= a 7 adicionar 10 de força.
 	if((senha.length >= 4) && (senha.length <= 7)){
 		forca += 10;
 	}
-	
+	//se for mais de 7 letras adicionar 25 de força.
 	else if(senha.length > 7){
 		forca += 25;
 	}
@@ -30,12 +30,12 @@ function validarSenhaForca(){
 		forca += 20;
 	}
 
-	
+	//Se conter caracteres especiais + 25 pontos.
 	if((senha.length >= 7) && (senha.match(/[@#$%&;*]/))){
 		forca += 25;
 	}
 
-	
+	//se a senha apresentar números iguais, a força diminui.
 	if(senha.match(/([1-9]+)\1{1,}/)){
 		forca += -25;
 	}
